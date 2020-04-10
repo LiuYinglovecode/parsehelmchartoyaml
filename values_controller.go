fc556f81d39811bc9c2cbabef52ceca8709579ce
yw_fop21ziwmvisejw <liuying6161661@163.com>
2020-04-10 17:25:38 +0800

    Update values_controller.go

--- values_controller.go
+++ values_controller.go
@@ -1,38 +1,32 @@
 package main
 
 import (
+	"basic-gin2/pkg/util/flat"
 	"bytes"
-
-	"github.com/gin-gonic/gin"
-	"k8s.io/helm/pkg/chartutil"
-	"k8s.io/helm/pkg/timeconv"
-
-	//"github.com/stretchr/testify/assert"
-	//"k8s.io/helm/pkg/proto/hapi/chart"
+	"encoding/json"
 	"net/http"
 	"net/url"
 
-	"k8s.io/helm/pkg/manifest"
-	"k8s.io/helm/pkg/renderutil"
+	"github.com/gin-gonic/gin"
+	"k8s.io/helm/pkg/chartutil"
 
 	//"strconv"
 	"fmt"
 
 	hgetter "k8s.io/helm/pkg/getter"
-	helmchart "k8s.io/helm/pkg/proto/hapi/chart"
 )
 
 var repoURL = "https://kubernetes-charts.storage.googleapis.com/redis-9.3.0.tgz"
 
-// RemoteHelm type
+// RemoteHelm defined
 type RemoteHelm struct{}
 
-// NewRemoteHelm func
+// NewRemoteHelm defined
 func NewRemoteHelm() RemoteHelm {
 	return RemoteHelm{}
 }
 
-// Values print values
+// Values defined
 func (controller RemoteHelm) Values(c *gin.Context) {
 	u, err := url.Parse(repoURL)
 	if err != nil {
@@ -65,41 +59,19 @@ func (controller RemoteHelm) Values(c *gin.Context) {
 	// fmt.Println(string(b))
 
 	// print values
-	now := timeconv.Now()
-
-	renderOpts := renderutil.Options{
-		ReleaseOptions: chartutil.ReleaseOptions{
-			Name:      "values_release",
-			IsInstall: true,
-			IsUpgrade: false,
-			Time:      now,
-			Namespace: "default",
-		},
-		KubeVersion: "1.11.5",
-	}
-
-	config := &helmchart.Config{Raw: string(chart.Values.Raw), Values: map[string]*helmchart.Value{}}
-	renderedTemplates, err := renderutil.Render(chart, config, renderOpts)
+	vl := chartutil.FromYaml(chart.Values.Raw)
+	f, err := flat.Flatten(vl, nil)
 	if err != nil {
-		c.Err()
+		c.Error(err)
 	}
-	listManifests := manifest.SplitManifests(renderedTemplates)
-	for _, manifest := range listManifests {
-		fmt.Println(chartutil.ToYaml(manifest))
+	v, err := json.MarshalIndent(f, " ", "\t")
+	if err != nil {
+		c.Error(err)
 	}
-
-	//vl := chartutil.FromYaml(chart.Values.Raw)
-	// f, err := flat.Flatten(vl, nil)
-	// if err != nil {
-	// 	c.Error(err)
-	// }
-	// v, err := json.MarshalIndent(f, " ", "\t")
-	// if err != nil {
-	// 	c.Error(err)
-	// }
 	//fmt.Println(string(v))
-	//m := pretty.Pretty(v)
-	//fmt.Println(string(m))
+	// m := pretty.Pretty(v)
+	//m := "[\n" + (v) + "]"
+	fmt.Println(string(v))
 	c.String(http.StatusOK, "json已打印！")
 }
 
