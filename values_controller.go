34e6ad3eca73a04ad526e6586266c97daac51c53
yw_fop21ziwmvisejw <liuying6161661@163.com>
2020-04-10 10:46:40 +0800

    去掉自己写的部分

--- values_controller.go
+++ values_controller.go
@@ -1,29 +1,38 @@
 package main
 
 import (
-	"basic-gin2/pkg/util/flat"
 	"bytes"
-	"encoding/json"
+
 	"github.com/gin-gonic/gin"
-	"github.com/tidwall/pretty"
 	"k8s.io/helm/pkg/chartutil"
+	"k8s.io/helm/pkg/timeconv"
+
+	//"github.com/stretchr/testify/assert"
+	//"k8s.io/helm/pkg/proto/hapi/chart"
 	"net/http"
 	"net/url"
+
+	"k8s.io/helm/pkg/manifest"
+	"k8s.io/helm/pkg/renderutil"
 
 	//"strconv"
 	"fmt"
 
 	hgetter "k8s.io/helm/pkg/getter"
+	helmchart "k8s.io/helm/pkg/proto/hapi/chart"
 )
 
 var repoURL = "https://kubernetes-charts.storage.googleapis.com/redis-9.3.0.tgz"
 
+// RemoteHelm type
 type RemoteHelm struct{}
 
+// NewRemoteHelm func
 func NewRemoteHelm() RemoteHelm {
 	return RemoteHelm{}
 }
 
+// Values print values
 func (controller RemoteHelm) Values(c *gin.Context) {
 	u, err := url.Parse(repoURL)
 	if err != nil {
@@ -56,18 +65,41 @@ func (controller RemoteHelm) Values(c *gin.Context) {
 	// fmt.Println(string(b))
 
 	// print values
-	vl := chartutil.FromYaml(chart.Values.Raw)
-	f, err := flat.Flatten(vl, nil)
-	if err != nil {
-		c.Error(err)
+	now := timeconv.Now()
+
+	renderOpts := renderutil.Options{
+		ReleaseOptions: chartutil.ReleaseOptions{
+			Name:      "values_release",
+			IsInstall: true,
+			IsUpgrade: false,
+			Time:      now,
+			Namespace: "default",
+		},
+		KubeVersion: "1.11.5",
 	}
-	v, err := json.MarshalIndent(f, " ", "\t")
+
+	config := &helmchart.Config{Raw: string(chart.Values.Raw), Values: map[string]*helmchart.Value{}}
+	renderedTemplates, err := renderutil.Render(chart, config, renderOpts)
 	if err != nil {
-		c.Error(err)
+		c.Err()
 	}
+	listManifests := manifest.SplitManifests(renderedTemplates)
+	for _, manifest := range listManifests {
+		fmt.Println(chartutil.ToYaml(manifest))
+	}
+
+	//vl := chartutil.FromYaml(chart.Values.Raw)
+	// f, err := flat.Flatten(vl, nil)
+	// if err != nil {
+	// 	c.Error(err)
+	// }
+	// v, err := json.MarshalIndent(f, " ", "\t")
+	// if err != nil {
+	// 	c.Error(err)
+	// }
 	//fmt.Println(string(v))
-	m := pretty.Pretty(v)
-	fmt.Println(string(m))
+	//m := pretty.Pretty(v)
+	//fmt.Println(string(m))
 	c.String(http.StatusOK, "json已打印！")
 }
 
